b3ac47c4d2913bcab9af1ae8fd1a67b703b89a20
RİFAT EMRE TUFAN <61663804+tufan4@users.noreply.github.com>
2026-02-08 17:52:40 +0300

    chore: revert to nodejs runtime for powerful scraping && feat: starting smart curriculum generator

--- app/api/deep-dive/route.ts
+++ app/api/deep-dive/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import * as cheerio from 'cheerio';
 
 export const dynamic = 'force-dynamic'; // No caching
-export const runtime = 'edge';
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
